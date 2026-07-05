fbdddb6ddad589c0c2a81df327f5fb28181d9ff1
PaoPaoHow <gavinpp2019@gmail.com>
2026-07-05 09:40:43 -0400

    Update tests_1c.py

--- tests/tests_1c.py
+++ tests/tests_1c.py
@@ -3,19 +3,19 @@ import pytest
 from labs.lab_1.lab_1c import max_subarray_sum
 
 
-def same_numbers():
+def test_same_numbers():
     assert max_subarray_sum([1, 1, 1, 1, 1]) == 5
     assert max_subarray_sum([0, 0, 0, 0]) == 0
-    assert max_subarray_sum([-1, -1, -1]) == -3
+    assert max_subarray_sum([-1, -1, -1]) == -1
 
-def alternating():
-    assert max_subarray_sum([1, 0, 1, 0, 1]) == 1
-    assert max_subarray_sum([-1, 0, -1, 0, -1]) == -1
+def test_alternating():
+    assert max_subarray_sum([1, 0, 1, 0, 1]) == 3
+    assert max_subarray_sum([-1, 0, -1, 0, -1]) == 0
 
-def opposites():
+def test_opposites():
     assert max_subarray_sum([-1, 1, -1, 1]) == 1
 
-def randoms():
+def test_randoms():
     assert max_subarray_sum([2, -1, 9, -2, 1, -4, 3]) == 10
 
 
